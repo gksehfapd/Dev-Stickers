f32ad6028010519b235d989f8f2a55e4bf58d512
gksehfapd <1ehfapd@gmail.com>
2023-07-06 13:16:09 +0900

    feat: Static Query

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/Seo'
 
 const Blog = () => {
+	const [blogPosts, setBlogPosts] = useState()
+
 	return (
 		<Layout title="Welcome to DevStickers ⭐️">
 			<p>the most Recent News from my shop</p>
+			{blogPosts}
 		</Layout>
 	)
 }
